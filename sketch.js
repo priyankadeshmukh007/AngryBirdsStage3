dc664c3fff8853d9b6d383e9b470a04374c3581b
Priyanka Deshmukh <deshmukhpriyanka738@gmail.com>
2021-01-27 11:34:29 +0530

    add constraint to bird & log

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var constraintLog;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -13,6 +15,8 @@ function preload() {
 function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
+
+    // angry bird game 
     world = engine.world;
 
 
@@ -35,6 +39,22 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+
+    constraintLog = new Log(230,180,80, PI/2);
+
+    var options = {
+
+        bodyA: bird.body,
+        bodyB: constraintLog.body,
+        stiffness: 0.04,
+        length: 10 
+
+
+    }
+
+    var chain = Constraint.create(options);
+    World.add(world,chain);
+    
 
 }
 
@@ -61,4 +81,8 @@ function draw(){
 
     bird.display();
     platform.display();
+    constraintLog.display();
+
+    strokeWeight(3);
+    line(bird.body.position.x, bird.body.position.y, constraintLog.body.position.x,constraintLog.body.position.y);
 }
